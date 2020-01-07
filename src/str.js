cd681e1aed5b6309dc356231232308ca810ff80e
Marcus Pöhls <marcus.poehls@gmail.com>
2020-01-07 15:50:23 +0100

    reorder methods

--- src/str.js
+++ src/str.js
@@ -48,164 +48,6 @@ class Str {
   }
 
   /**
-   * Uppercases the string.
-   *
-   * @returns {Str}
-   */
-  upper () {
-    return this.toUpperCase()
-  }
-
-  /**
-   * Uppercases the string. Alias for `.upper()`.
-   *
-   * @returns {Str}
-   */
-  uppercase () {
-    return this.toUpperCase()
-  }
-
-  /**
-   * Uppercases the string. Alias for `.upper()` and convenience
-   * method to comply with the global String’s `.toUpperCase()`.
-   *
-   * @returns {Str}
-   */
-  toUpperCase () {
-    return new Str(
-      this.value.toUpperCase()
-    )
-  }
-
-  /**
-   * Determine whether the given string is uppercase.
-   *
-   * @returns {Boolean}
-   */
-  isUpper () {
-    return this.isUppercase()
-  }
-
-  /**
-   * Determine whether the given string is uppercase.
-   *
-   * @returns {Boolean}
-   */
-  isUppercase () {
-    return this.value === this.upper().get()
-  }
-
-  /**
-   * Lowercases the string.
-   *
-   * @returns {Str}
-   */
-  lower () {
-    return this.toLowerCase()
-  }
-
-  /**
-   * Lowercases the string. Alias for `.lower()`.
-   *
-   * @returns {Str}
-   */
-  lowercase () {
-    return this.toLowerCase()
-  }
-
-  /**
-   * Lowercases the string. Alias for `.lower()` and convenience
-   * method to comply with the global String’s `.toLowerCase()`.
-   *
-   * @returns {Str}
-   */
-  toLowerCase () {
-    return new Str(
-      this.value.toLowerCase()
-    )
-  }
-
-  /**
-   * Determine whether the string is lowercase.
-   *
-   * @returns {Boolean}
-   */
-  isLower () {
-    return this.isLowercase()
-  }
-
-  /**
-   * Determine whether the string is lowercase.
-   *
-   * @returns {Boolean}
-   */
-  isLowercase () {
-    return this.value === this.lower().get()
-  }
-
-  /**
-   * Lowercases the first character in the string.
-   *
-   * @returns {Str}
-   */
-  lcFirst () {
-    return new Str(
-      this.value[0].toLowerCase() + this.value.slice(1)
-    )
-  }
-
-  /**
-   * Removes whitespace around the string, front and back.
-   *
-   * @returns {Str}
-   */
-  trim () {
-    return new Str(
-      this.value.trim()
-    )
-  }
-
-  /**
-   * Removes all whitespace from the string, everywhere.
-   *
-   * @returns {Str}
-   */
-  strip () {
-    return new Str(
-      this.value.replace(/\s+/g, '')
-    )
-  }
-
-  /**
-   * Splits up the string into an array of strings by separating
-   * the string at each of the specified `separator` string.
-   *
-   * @param {String} separator
-   * @param {Number} limit
-   *
-   * @returns {Array}
-   */
-  split (separator, limit) {
-    return this.value.split(separator, limit)
-  }
-
-  /**
-   * Convert the string to title case.
-   *
-   * @returns {Str}
-   */
-  title () {
-    return new Str(
-      this
-        .lowercase()
-        .split(' ')
-        .filter(s => s)
-        .map(s => s[0].toUpperCase() + s.slice(1))
-        .join(' ')
-    )
-  }
-
-  /**
    * Convert the string to camelCase.
    *
    * @returns {Str}
@@ -214,46 +56,6 @@ class Str {
     return new Str(
       this.studly().lcFirst().get()
     )
-  }
-
-  /**
-   * Convert the string to StudlyCase. StudlyCase is
-   * similar to camelCase except that the first
-   * character is uppercase instead of lowercase.
-   *
-   * @returns {Str}
-   */
-  studly () {
-    return new Str(
-      this.value.replace(/[_-]+/g, ' ')
-    ).title().strip()
-  }
-
-  /**
-   * Create a UUID (version 4).
-   *
-   * @returns {String}
-   */
-  static uuid () {
-    return Uuid()
-  }
-
-  /**
-   * Create a random, URL-friendly string. The default length will have 21 symbols.
-   *
-   * @param {Number} [size=21] number of symbols in string
-   *
-   * @returns {String}
-   */
-  static random (size = 21) {
-    const bytes = Crypto.randomBytes(size)
-    let random = ''
-
-    while (size--) {
-      random += Str.alphabet[bytes[size] & 63]
-    }
-
-    return random
   }
 
   /**
@@ -281,15 +83,6 @@ class Str {
   }
 
   /**
-   * Returns the string’s length.
-   *
-   * @returns {Number}
-   */
-  length () {
-    return this.value.length
-  }
-
-  /**
    * Determine whether the given string is empty.
    *
    * @returns {Boolean}
@@ -299,12 +92,221 @@ class Str {
   }
 
   /**
+   * Determine whether the string is lowercase.
+   *
+   * @returns {Boolean}
+   */
+  isLower () {
+    return this.isLowercase()
+  }
+
+  /**
+   * Determine whether the string is lowercase.
+   *
+   * @returns {Boolean}
+   */
+  isLowercase () {
+    return this.value === this.lower().get()
+  }
+
+  /**
    * Determine whether the given string is not empty.
    *
    * @returns {Boolean}
    */
   isNotEmpty () {
     return !this.isEmpty()
+  }
+
+  /**
+   * Determine whether the given string is uppercase.
+   *
+   * @returns {Boolean}
+   */
+  isUpper () {
+    return this.isUppercase()
+  }
+
+  /**
+   * Determine whether the given string is uppercase.
+   *
+   * @returns {Boolean}
+   */
+  isUppercase () {
+    return this.value === this.upper().get()
+  }
+
+  /**
+   * Lowercases the first character in the string.
+   *
+   * @returns {Str}
+   */
+  lcFirst () {
+    return new Str(
+      this.value[0].toLowerCase() + this.value.slice(1)
+    )
+  }
+
+  /**
+   * Returns the string’s length.
+   *
+   * @returns {Number}
+   */
+  length () {
+    return this.value.length
+  }
+
+  /**
+   * Lowercases the string.
+   *
+   * @returns {Str}
+   */
+  lower () {
+    return this.toLowerCase()
+  }
+
+  /**
+   * Lowercases the string. Alias for `.lower()`.
+   *
+   * @returns {Str}
+   */
+  lowercase () {
+    return this.toLowerCase()
+  }
+
+  /**
+   * Create a random, URL-friendly string. The default length will have 21 symbols.
+   *
+   * @param {Number} [size=21] number of symbols in string
+   *
+   * @returns {String}
+   */
+  static random (size = 21) {
+    const bytes = Crypto.randomBytes(size)
+    const alphabetLength = Str.alphabet.length
+
+    let random = ''
+
+    while (size--) {
+      random += Str.alphabet[bytes[size] & alphabetLength]
+    }
+
+    return random
+  }
+
+  /**
+   * Splits up the string into an array of strings by separating
+   * the string at each of the specified `separator` string.
+   *
+   * @param {String} separator
+   * @param {Number} limit
+   *
+   * @returns {Array}
+   */
+  split (separator, limit) {
+    return this.value.split(separator, limit)
+  }
+
+  /**
+   * Removes all whitespace from the string, everywhere.
+   *
+   * @returns {Str}
+   */
+  strip () {
+    return new Str(
+      this.value.replace(/\s+/g, '')
+    )
+  }
+
+  /**
+   * Convert the string to StudlyCase. StudlyCase is
+   * similar to camelCase except that the first
+   * character is uppercase instead of lowercase.
+   *
+   * @returns {Str}
+   */
+  studly () {
+    return new Str(
+      this.value.replace(/[_-]+/g, ' ')
+    ).title().strip()
+  }
+
+  /**
+   * Convert the string to title case.
+   *
+   * @returns {Str}
+   */
+  title () {
+    return new Str(
+      this
+        .lowercase()
+        .split(' ')
+        .filter(s => s)
+        .map(s => s[0].toUpperCase() + s.slice(1))
+        .join(' ')
+    )
+  }
+
+  /**
+   * Lowercases the string. Alias for `.lower()` and convenience
+   * method to comply with the global String’s `.toLowerCase()`.
+   *
+   * @returns {Str}
+   */
+  toLowerCase () {
+    return new Str(
+      this.value.toLowerCase()
+    )
+  }
+
+  /**
+   * Uppercases the string. Alias for `.upper()` and convenience
+   * method to comply with the global String’s `.toUpperCase()`.
+   *
+   * @returns {Str}
+   */
+  toUpperCase () {
+    return new Str(
+      this.value.toUpperCase()
+    )
+  }
+
+  /**
+   * Removes whitespace around the string, front and back.
+   *
+   * @returns {Str}
+   */
+  trim () {
+    return new Str(
+      this.value.trim()
+    )
+  }
+
+  /**
+   * Uppercases the string.
+   *
+   * @returns {Str}
+   */
+  upper () {
+    return this.toUpperCase()
+  }
+
+  /**
+   * Uppercases the string. Alias for `.upper()`.
+   *
+   * @returns {Str}
+   */
+  uppercase () {
+    return this.toUpperCase()
+  }
+
+  /**
+   * Create a UUID (version 4).
+   *
+   * @returns {String}
+   */
+  static uuid () {
+    return Uuid()
   }
 }
 
