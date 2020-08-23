2a07c929eaa710206aa836f8341776630514b4fd
Marcus Pöhls <marcus.poehls@gmail.com>
2020-08-23 14:32:41 +0200

    move entrypoint to dist

--- index.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-const Str = require('./dist')
-
-module.exports = Str
--- test/str.js
+++ test/str.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const Str = require('..')
+const Str = require('../dist')
 
 describe('Strings', () => {
   it('get', () => {
